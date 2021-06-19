82edd4aeacb67979b2c04baa106114c4dfcc34ff
Dumitrash <saratinean1.9@gmail.com>
2021-06-19 12:39:28 +0300

    actualizarea

--- img2/server.js
+++ img2/server.js
@@ -1,9 +1,16 @@
+ 
 var http = require('http');
-
+var startTime;
+var host;
 var handleRequest = function(request, response) {
-  console.log('Hello World' + request.url);
+  response.setHeader('Content-Type', 'text/plain');
   response.writeHead(200);
-  response.write("Hello World ! ");
-};
+  response.write("Hello ,am actualizat aplicatia ! | Ruleaza pe :");
+  response.write(host);
+  response.end(" v=2\n");
+  console.log("Running On:" , host);
+}
 var www = http.createServer(handleRequest);
-www.listen(8080);
+www.listen(8080,function () {
+    host = process.env.HOSTNAME;
+});
